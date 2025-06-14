c05f5f911845c7ecd7bb4a070df2a0ee30fbea3d
Jelly💖 <enquiries.fractured@gmail.com>
2025-06-14 17:33:23 +0100

    Update main.js

--- artists/main.js
+++ artists/main.js
@@ -41,26 +41,6 @@ if (isMobile) {
 })();
 
 
-// Sparkly mouse trail logic
-window.addEventListener('mousemove', function(e) {
-  const arr = [1, 0.9, 0.8, 0.5, 0.2];
-
-  arr.forEach(function(i) {
-    const x = (1 - i) * 75;
-    const star = document.createElement('div');
-    star.className = 'star';
-
-    star.style.top = e.clientY + Math.round(Math.random() * x - x / 2) + 'px';
-    star.style.left = e.clientX + Math.round(Math.random() * x - x / 2) + 'px';
-
-    document.body.appendChild(star);
-
-    window.setTimeout(function() {
-      document.body.removeChild(star);
-    }, Math.round(Math.random() * i * 600));
-  });
-}, false);
-
 document.addEventListener('DOMContentLoaded', () => {
   // Sparkle effects for avatars
   const wrappers = document.querySelectorAll('.artist-avatar-wrapper');
